cf5e4360a0000167d571bb8dd8bb514fb65f3932
trevor.liloia <trevor.liloia@students.aie.edu.au>
2016-10-11 12:58:32 -0700

    mat3

---
+++ MathLib/mat3.cpp
@@ -0,0 +1,95 @@
+#include "mat3.h"
+#include "flops.h"
+#include "vec3.h"
+
+vec3 mat3::operator[](unsigned idx) const
+{
+	return c[idx];
+}
+
+vec3 & mat3::operator[](unsigned idx)
+{
+	return c[idx];
+}
+
+bool operator==(const mat3 &lhs, const mat3 &rhs)
+{
+	return fequals(lhs.m[0], rhs.m[0]) &&
+		fequals(lhs.m[1], rhs.m[1]) &&
+		fequals(lhs.m[2], rhs.m[2]) &&
+		fequals(lhs.m[3], rhs.m[3]) &&
+		fequals(lhs.m[4], rhs.m[4]) &&
+		fequals(lhs.m[5], rhs.m[5]) &&
+		fequals(lhs.m[6], rhs.m[6]) &&
+		fequals(lhs.m[7], rhs.m[7]) &&
+		fequals(lhs.m[8], rhs.m[8]);
+}
+
+mat3 operator+(const mat3 &lhs, const mat3 &rhs)
+{
+	return mat3{ lhs.m[0] + rhs.m[0], lhs.m[1] + rhs.m[1],
+		lhs.m[2] + rhs.m[2], lhs.m[3] + rhs.m[3], lhs.m[4] + rhs.m[4],
+		lhs.m[5] + rhs.m[5], lhs.m[6] + rhs.m[6], lhs.m[7] + rhs.m[7], lhs.m[8] + rhs.m[8]};
+}
+mat3 operator-(const mat3 &lhs, const mat3 &rhs)
+{
+	return mat3{ lhs.m[0] - rhs.m[0], lhs.m[1] - rhs.m[1],
+		lhs.m[2] - rhs.m[2], lhs.m[3] - rhs.m[3], lhs.m[4] - rhs.m[4],
+		lhs.m[5] - rhs.m[5], lhs.m[6] - rhs.m[6], lhs.m[7] - rhs.m[7], lhs.m[8] - rhs.m[8] };
+}
+mat3 operator-(const mat3 &lhs)
+{
+	return mat3{ -lhs.m[0], -lhs.m[1], -lhs.m[2], -lhs.m[3],-lhs.m[4], -lhs.m[5], -lhs.m[6], -lhs.m[7],-lhs.m[8]};
+}
+mat3 transpose(const mat3 &lhs)
+{
+	return mat3{ lhs.m[0], lhs.m[3], lhs.m[6], lhs.m[1], lhs.m[4], lhs.m[7], lhs.m[2], lhs.m[5], lhs.m[8] };
+}
+mat3 operator*(const mat3 &lhs, float rhs)
+{
+	return mat3{ lhs.m[0] *rhs,lhs.m[1] * rhs, lhs.m[2] * rhs, lhs.m[3] * rhs,lhs.m[4] * rhs, lhs.m[5] * rhs, lhs.m[6] * rhs, lhs.m[7] * rhs,lhs.m[8] * rhs };
+}
+mat3 operator*(float lhs, const mat3 &rhs)
+{
+	return rhs * lhs;
+}
+mat3 operator*(const mat3 &lhs, const mat3 &rhs)
+{
+	return mat3{ ((lhs.m[0] * rhs.m[0]) + (lhs.m[3] * rhs.m[1]) + (lhs.m[6] * rhs.m[2])),
+				((lhs.m[1] * rhs.m[0]) + (lhs.m[4] * rhs.m[1]) + (lhs.m[7] * rhs.m[2])),
+				((lhs.m[2] * rhs.m[0]) + (lhs.m[5] * rhs.m[1]) + (lhs.m[8] * rhs.m[2])),
+
+				((lhs.m[0] * rhs.m[3]) + (lhs.m[3] * rhs.m[4]) + (lhs.m[6] * rhs.m[5])),
+				((lhs.m[1] * rhs.m[3]) + (lhs.m[4] * rhs.m[4]) + (lhs.m[7] * rhs.m[5])),
+				((lhs.m[2] * rhs.m[3]) + (lhs.m[5] * rhs.m[4]) + (lhs.m[8] * rhs.m[5])),
+
+				((lhs.m[0] * rhs.m[6]) + (lhs.m[3] * rhs.m[7]) + (lhs.m[6] * rhs.m[8])),
+				((lhs.m[1] * rhs.m[6]) + (lhs.m[4] * rhs.m[7]) + (lhs.m[7] * rhs.m[8])),
+				((lhs.m[2] * rhs.m[6]) + (lhs.m[5] * rhs.m[7]) + (lhs.m[8] * rhs.m[8])) };
+}
+
+// 3x[3 * 3]x1 = 3x1
+vec3 operator*(const mat3 &lhs, const vec3 &rhs)
+{
+	return vec3{ lhs.m[0] * rhs.x + lhs.m[3] * rhs.y + lhs.m[6] * rhs.z, lhs.m[1] * rhs.x + lhs.m[4] * rhs.y + lhs.m[7] * rhs.z, lhs.m[2] * rhs.x + lhs.m[5] * rhs.y + lhs.m[8] * rhs.z };
+}
+
+float determinant(const mat3 & lhs)
+{
+	return (lhs.m[0] * lhs.m[4] * lhs.m[8] +
+		lhs.m[3] * lhs.m[7] * lhs.m[2] +
+		lhs.m[6] * lhs.m[1] * lhs.m[5] -
+		lhs.m[6] * lhs.m[4] * lhs.m[2] -
+		lhs.m[3] * lhs.m[1] * lhs.m[8] -
+		lhs.m[0] * lhs.m[5] * lhs.m[7]);
+}
+
+mat3 inverse(const mat3 & lhs)
+{
+	return (1 / determinant(lhs)) * transpose(lhs);
+}
+
+mat3 mat3Identity()
+{
+	return mat3{ 1,0,0,0,1,0,0,0,1 };
+}
--- MathLib/mat3.h
+++ MathLib/mat3.h
@@ -1,1 +1,28 @@
 #pragma once
+#include "vec3.h"
+
+union mat3
+{
+	float m[9];
+	float mm[3][3];
+	vec3 c[3];
+	vec3 operator[](unsigned idx) const;
+	vec3 &operator[](unsigned idx);
+};
+
+
+
+bool operator==(const mat3 &lhs, const mat3 &rhs);
+mat3 operator+(const mat3 &lhs, const mat3 &rhs);
+mat3 operator-(const mat3 &lhs, const mat3 &rhs);
+mat3 operator-(const mat3 &lhs);
+mat3 transpose(const mat3 &lhs);
+
+mat3 operator*(const mat3 &lhs, float rhs);
+mat3 operator*(float lhs, const mat3 &rhs);
+mat3 operator*(const mat3 &lhs, const mat3 &rhs);
+vec3 operator*(const mat3 &lhs, const vec3 &rhs);
+
+float determinant(const mat3 &lhs);
+mat3 inverse(const mat3 &lhs);
+mat3 mat3Identity();
